ad4eed626533671316e293d733017eedba24c7e0
Babylyn <190585086+m1ra-san@users.noreply.github.com>
2025-07-25 18:00:29 +0800

    Fix hovering, create random color generator and color picker and better UI design

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const canvas=document.querySelector(".canvas");
-const selectSize=document.querySelector(".size-selector")
-
+const selectSize=document.querySelector(".size-selector");
+const colorPicker=document.querySelector("#colorPicker");
+const prismatic=document.querySelector("#prismaticColor")
+let useRandomColor = false;
 
 function createPixels(pixel){
     for (let i=0;i<(pixel*pixel);i++){
         const newPixel=document.createElement("div")
+            newPixel.addEventListener('mouseenter',setColor);
         canvas.appendChild(newPixel);
         newPixel.classList.add("gridSizepixel");
         console.log("added")
@@ -12,6 +15,32 @@ function createPixels(pixel){
     canvas.style.gridTemplateColumns = `repeat(${pixel}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${pixel}, 1fr)`;
 }
+
+
+function getRandomColor(){
+    var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function setColor(e){
+    if(useRandomColor){
+        e.target.style.backgroundColor = getRandomColor();
+    } else {
+        e.target.style.backgroundColor = colorPicker.value;
+    }
+
+}
+
+prismatic.addEventListener("click", (e)=>{
+    useRandomColor = !useRandomColor;
+    prismatic.classList.toggle("active-mode", useRandomColor);
+})
+
 
 function deletePriv(){
     canvas.innerHTML = ""; 
@@ -26,3 +55,5 @@ selectSize.addEventListener("change",(e)=>{
 })
 
 
+let gridDefault=16;
+createPixels(gridDefault);
